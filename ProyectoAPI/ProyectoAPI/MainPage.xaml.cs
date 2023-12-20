97f0b27f6e3a6254a296d9e652fb2bc3b2ed4597
miguel <mg20101994@gmail.com>
2023-12-20 18:21:13 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoAPI/ProyectoAPI/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+namespace ProyectoAPI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public class DemoAPI
+        {
+            public string id { get; set; }
+            public string url { get; set; }
+            public int width { get; set; }
+            public int height { get; set; }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://api.thecatapi.com/v1/images/search?limit=10");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accpet", "application/json");
+
+            /*Enviar la solicitud*/
+
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<List<DemoAPI>>(content);
+                ListDemo.ItemsSource = resultado;
+
+            }
+        }
+    }
+}
